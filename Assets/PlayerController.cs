a878ed1cf3daed92d4e61c61ca5ccce284502936
Jasmineleigh <89361875+Jasmineleigh@users.noreply.github.com>
2023-02-07 16:16:23 -0500

    Add files via upload

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,84 +1,22 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-// Takes and handles input and movement for a player character
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 1f;
-    public float collisionOffset = 0.05f;
-    public ContactFilter2D movementFilter;
+    public Animator animator;
 
-    Vector2 movementInput;
-    Rigidbody2D rb;
-    Animator animator;
-    SpriteRenderer spriteRenderer;
-    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
-
-
-    // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-
     }
 
-    private void FixedUpdate() {
-        float xDirection = movementInput.x;
-        float yDirection = movementInput.y;
+    void Update(){
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Magnitude", movement.magnitude);
+        transform.position = transform.position + movement * Time.deltaTime;
 
-        if(movementInput != Vector2.zero) {
-            bool success = TryMove(movementInput);
-
-            if(!success){
-                success =TryMove(new Vector2(movementInput.x, 0));
-                if(!success){
-                    success = TryMove(new Vector2(movementInput.y, 0));
-                }
-            }
-            
-            animator.SetBool("isMoving", success);
-             
-        }
-        else{
-            animator.SetBool("isMoving", false);
-        }
-
-        // Set Direction to movement direction
-        if(movementInput.x < 0){
-            spriteRenderer.flipX  = true;
-        }
-        else if(movementInput.x > 0){
-            spriteRenderer.flipX = false;
-        }
-        
-       
-        
-    }
-
-    private bool TryMove(Vector2 direction){
-        int count = rb.Cast(
-                direction, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
-                movementFilter, // The settings that determine where a collision can occur on such as layers to collide with
-                castCollisions, // List of collisions to store the found collisions into after the Cast is finished
-                moveSpeed * Time.fixedDeltaTime + collisionOffset); // The amount to cast equal to the movement plus an offset
-
-        if(count == 0){
-            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
-            return true;
-        }
-        else{
-           return false;
-        }
-    }
-
-    void OnMove(InputValue movementValue) {
-        movementInput = movementValue.Get<Vector2>();
     }
 }
-
-   
