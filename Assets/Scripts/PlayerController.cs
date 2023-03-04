aa545965f7a809518839bfaf9baf83b93aee1f34
Jasmineleigh <89361875+Jasmineleigh@users.noreply.github.com>
2023-03-03 23:27:27 -0500

    Add Gathering

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,26 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{
+{   // Player Components
     public Animator animator;
     public Rigidbody2D rbPlayer;
-    // public Collider2D tmcTrees;
     public Collider2D colPlayer;
     public float speed = 5.0f;
+
     public string sideCollision = "";
     
     // Bounds of player on map
     public Transform topLeft;
     public Transform bottomRight;
     private float xMin, xMax, yMin, yMax;
-
-    public bool isTouchingTree = false;
+    // Tree Chopping Vars
     public GameObject tree;
-    // public bool choppingTree = false;
-    // private float timeToChop = 0.25f;
-    // private float timer = 0f;
     public ButtonController btn;
-
+    //Search
+    public GameObject building;
     void Start(){
         animator = GetComponent<Animator>();
         rbPlayer = GetComponent<Rigidbody2D>();
@@ -45,7 +42,7 @@ public class PlayerController : MonoBehaviour
         this.transform.position = new Vector3(Mathf.Clamp(transform.position.x, xMin, xMax),Mathf.Clamp(transform.position.y, yMin, yMax), this.transform.position.z);
 
     }
-
+    // Performed when player collides with an object
     private void OnCollisionEnter2D(Collision2D collision){
         // Debug.Log(collision.gameObject.name + " : " + gameObject.name + " : " + Time.time);
         if(collision.gameObject.tag == "Tree"){
@@ -56,11 +53,25 @@ public class PlayerController : MonoBehaviour
             btn.button.SetActive(true);
             CheckCollidingSide(btn.tree);
       
+        }else if(collision.gameObject.tag == "Neighbor"){
+            //Search
+            building = collision.gameObject;
+            //Random number of items (food, scrap, rarely water)
+            //No more than +10 items in total
+        }else if(collision.gameObject.tag == "LakeHouse"){
+            //Fish (food, water)
+            building = collision.gameObject;
+            //No more than +3 fish 
+        }else if(collision.gameObject.tag == "Wind"){
+            //Search (water, scrap, rare item)
+            building = collision.gameObject;
         }
+
     }
 
-    private void CheckCollidingSide(GameObject obj){
-        if(transform.position.x > obj.transform.position.x){
+    // Check which side a tree object collides with the character
+    private void CheckCollidingSide(GameObject tree){
+        if(transform.position.x > tree.transform.position.x){
             Debug.Log("Right side collision");
             sideCollision = "right";
         }else{
@@ -68,7 +79,7 @@ public class PlayerController : MonoBehaviour
             sideCollision = "left";
         }
     }
-
+    // Performed when the player stops colliding with an object
     private void OnCollisionExit2D(Collision2D collision){
         // Debug.Log(collision.gameObject.name + " : " + gameObject.name + " : " + Time.time);
         if(collision.gameObject.tag == "Tree"){
@@ -76,6 +87,12 @@ public class PlayerController : MonoBehaviour
            
             ButtonController btn = Camera.main.GetComponent<ButtonController>();
             btn.button.SetActive(false);
+        }else if(collision.gameObject.tag == "Neighbor"){
+           //Deactivate Search
+        }else if(collision.gameObject.tag == "LakeHouse"){
+            //Deactivate Fish
+        }else if(collision.gameObject.tag == "Wind"){
+            //Deactivate Search
         }
     }
 
