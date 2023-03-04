c2efabaf3a1cae44b01eb5dc0490c3d434cb1506
Jasmineleigh <89361875+Jasmineleigh@users.noreply.github.com>
2023-03-03 23:28:31 -0500

    Added checks and added wood

--- Assets/Scripts/Home.cs
+++ Assets/Scripts/Home.cs
@@ -7,11 +7,12 @@ public class Home : MonoBehaviour
     public static double food = 0;
     public static double water = 0;
     public static double scrap = 0;
-    
+    public float wood = 0; 
+    public GameObject house;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Food= " + food + " and Scrap= " + scrap);
+        Debug.Log("Food= " + food + ", Scrap= " + scrap + ", Wood= " + wood + ", Water= " + water) ;
         // Calls the method to change the amount of resources to simulate passive change. Calls the method after 60 seconds, then repeats every 60 seconds
         InvokeRepeating("Passive_Resource_Change", 60.0f, 60.0f);
 
@@ -22,14 +23,18 @@ public class Home : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        Debug.Log("Food= " + food + ", Scrap= " + scrap + ", Wood= " + wood + ", Water= " + water) ;
     }
 
     //Method to control the amount of food, water, and scrap gain/loss. Default is 1 per method call
     void Passive_Resource_Change(){
-        food -= 1;
-        water -= 1;
+        if(food > 0)
+            food -= 1;
+        if(water >0)
+            water -= 1;
         scrap += 1;
+        if(wood > 0)
+            wood -=1;
     }
 
     void Show(){
