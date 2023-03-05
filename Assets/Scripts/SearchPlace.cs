7cf35c5f0a0ebffa4e2f88b6f383aeb98f02bb80
Jasmineleigh <89361875+Jasmineleigh@users.noreply.github.com>
2023-03-04 21:27:12 -0500

    Updated searching house button

--- Assets/Scripts/SearchPlace.cs
+++ Assets/Scripts/SearchPlace.cs
@@ -11,7 +11,7 @@ public class SearchPlace : MonoBehaviour
     Animator animator;
 
     public float timer = 0.0f;
-    public float searchTime = 8.0f;
+    public float searchTime = 2.0f;
     public bool isSearching = false;
 
     // Start is called before the first frame update
@@ -24,6 +24,23 @@ public class SearchPlace : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(isSearching){
+            player.SetActive(false);
+            timer += Time.deltaTime;
+            if(timer > searchTime){
+                isSearching = false;
+                timer = 0.0f;
+                player.SetActive(true);
+                
+            }
+        }
         
+    }
+
+    public void OnClick(){
+        if(!isSearching){
+            player.GetComponent<PlayerController>().isSearching = true;
+            isSearching = true;
+        }
     }
 }
