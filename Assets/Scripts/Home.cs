d4e7e65d8c09da488442c0e549fedcc1a19d95a9
Jasmineleigh <89361875+Jasmineleigh@users.noreply.github.com>
2023-03-05 20:57:37 -0500

    Modify for resource gathering

--- Assets/Scripts/Home.cs
+++ Assets/Scripts/Home.cs
@@ -7,23 +7,27 @@ public class Home : MonoBehaviour
     public static double food = 0;
     public static double water = 0;
     public static double scrap = 0;
-    public float wood = 0; 
+    public static double wood = 0; 
     public GameObject house;
+    public GameObject button;
+    public bool visiting = false;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Food= " + food + ", Scrap= " + scrap + ", Wood= " + wood + ", Water= " + water) ;
+        // Debug.Log("Food= " + food + ", Scrap= " + scrap + ", Wood= " + wood + ", Water= " + water) ;
         // Calls the method to change the amount of resources to simulate passive change. Calls the method after 60 seconds, then repeats every 60 seconds
-        InvokeRepeating("Passive_Resource_Change", 60.0f, 60.0f);
+        InvokeRepeating("Passive_Resource_Change", 300.0f, 300.0f);
 
         // For testing purposes only, shows the amount of current resources every 5 seconds
-        InvokeRepeating("Show", 5.0f, 5.0f);
+        // InvokeRepeating("Show", 5.0f, 5.0f);
+
+        button.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Food= " + food + ", Scrap= " + scrap + ", Wood= " + wood + ", Water= " + water) ;
+        
     }
 
     //Method to control the amount of food, water, and scrap gain/loss. Default is 1 per method call
@@ -38,6 +42,15 @@ public class Home : MonoBehaviour
     }
 
     void Show(){
-        Debug.Log("Food = " + food + " && Scrap = " + scrap);
+        if(visiting){
+            Debug.Log("Food = " + food + ", Scrap = " + scrap + ", Water = " + water + ", Wood = " + wood);
+            visiting = false;
+        }
+    }
+
+    public void OnClick(){
+        //display resources
+        visiting = true;
+        Show();
     }
 }
